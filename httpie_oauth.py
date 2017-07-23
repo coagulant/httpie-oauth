c1a5f26fd019b71afe402e05c604886f8c8c0f36
Ilya Baryshev <baryshev@gmail.com>
2017-07-23 22:03:26 +0300

    Add oauth1.0 support with client key/secret
    Is not backwards compatible

--- httpie_oauth.py
+++ httpie_oauth.py
@@ -2,6 +2,7 @@
 OAuth plugin for HTTPie.
 
 """
+import os
 from httpie.plugins import AuthPlugin
 from requests_oauthlib import OAuth1
 
@@ -18,4 +19,7 @@ class OAuth1Plugin(AuthPlugin):
     description = ''
 
     def get_auth(self, username, password):
-        return OAuth1(client_key=username, client_secret=password)
+        client_key = os.environ.get('OAUTH_CLIENT_KEY', None)
+        client_secret = os.environ.get('OAUTH_CLIENT_SECRET', None)
+        return OAuth1(client_key=client_key, client_secret=client_secret,
+                      resource_owner_key=username, resource_owner_secret=password)
